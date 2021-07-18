9a123e47579ac78571a422f889c557fc52e00d23
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-18 23:01:48 +0100

    Revised JS and DOM exercises

--- index.js
+++ index.js
@@ -1,90 +1,122 @@
-/* JS EXERCISES
 
-21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
-22) Create an object with properties such name, surname, email
-23) Delete Email from the previously created object
-24) Create an array with 10 strings in it
-25) Print in the console every string in the previous array
-26) Create an array with 100 random numbers in it
-27) Write a function to get the MAX and the MIN from the previously created array
-28) Create an array of arrays, in which every array has 10 random numbers
+// 21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+
+const x = 'John'
+const y = 'Doe'
+console.log(`${x} <> ${y}`)
+
+/* JS EXERCISES
 29) Create a function that gets 2 arrays and returns the longest one
 30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
 */
 
+// 22) Create an object with properties such name, surname, email
+const person = {
+    name: 'Naruto',
+    surname: 'Uzumaki',
+    email: 'konohahokage@gmail.com',
+} 
 
+// 23) Delete Email from the previously created object
 
-concatinatedName("John", "Doe")
+delete person.email
 
-const personObject{
-    name: "Monkey D",
-    surname: "Luffy",
-    email: "mugiwaracaptain@outlook.com",
+// 24) Create an array with 10 strings in it
+const stringsArr = []
+
+for (let i = 1; i <= 10; i++) {
+    stringsArr.push("string-" + i)
 }
 
-delete personObject.email
-console.log(personObject)
+// 25) Print in the console every string in the previous array
+console.log(stringsArr)
 
-const heroRankings = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-console.log(heroRankings)
+// 26) Create an array with 100 random numbers in it
+const randomNumArr = []
 
-Array.from({length: 100}, () => Math.floor(Math.random() * 100));
-console.log(Array)
-
-const getExtremes = function(Array){
-let smallest = max(Array)
-let largest = min(Array)
-return smallest
-return largest
+for (let i = 0; i < 100; i++) {
+    randomNumArr.push(Math.floor(Math.random() *100))
 }
 
-getExtremes(Array)
+// 27) Write a function to get the MAX and the MIN from the previously created array
+function findMinAndMax(arr) {
 
-arrayOfArrays = [
-    [first.from(length: 10} () => Math.floor(Math.random() *10)),
-    second.from(length: 10} () => Math.floor(Math.random() *10)),
-    third.from(length: 10} () => Math.floor(Math.random() *10)),
-    fourth.from(length: 10} () => Math.floor(Math.random() *10))
-    ]
-]
+    console.log("FOR THE FUNCTION", arr)
+    
+    let min = arr[0]
+    let max = arr[0]
+    
+    for (let i = 0; i < arr.length; i++) {
+        let currNum = arr[i]
+        
+        if (currNum < min) {
+            min = currNum
+        }
 
-/* DOM 
+        if (currNum > max) {
+            max = currNum
+        }
+    }
+    
+    console.log("MIN VALUE IS", min)
+    console.log("MAX VALUE IS", max)
+}
 
-        39) Write a function to add new items to a UL
-        40) Write a function to empty a list */
-
-// Get element with ID "container" from the page
+findMinAndMax(randomNumArr)
 
 
-// Get every "td" from the page
 
-getTableContents = document.getElementsByTagName("td")
 
-// Create a cycle that prints the text inside every td of the page
+// 31) Get element with ID "container" from the page
+function container (){
+    console.log(document.querySelector("container"))
+}
 
-// Write a function to change the heading of the page
+// 32) Get every "td" from the page
 
-const changeHeading = getElementsByTagName("h1")
-changeHeading.textContent = "Pirating exercise"
+const getTableContents = document.getElementsByTagName("td")
 
-// Write a function to add an extra row to the table
+// 33) Create a cycle that prints the text inside every td of the page
+
+for (let i = 0; i < getTableContents.length; i++) {
+    console.log(getTableContents[i].innerText)
+    
+}
+
+// 34) Write a function to change the heading of the page
+function changeH1(string){
+    const pageHeading = getElementsByTagName("h1")[0]
+    pageHeading.innerHTML = string
+    
+}
+
+// 35) Write a function to add an extra row to the table
 
 HTMLTableElement.insertRow()
 
-// Write a function to add the class "test" to each row in the table
+// 36) Write a function to add the class "test" to each row in the table
 
 const tableRows = querySelectorAll("tr")
 tableRows.className += "test"
 
-// Write a function to add a red background to every link in the page
+// 37) Write a function to add a red background to every link in the page
 
 const redLink = querySelectorAll("a").style.backgroundColor = "red"
 
-pageLoaded = console.log("Page Loaded")
+// 38) Console log "Page loaded" when the page is correctly loaded
+window.onload = function () {
+    console.log("PAGE LOADED")
 
-onload = pageLoaded
+// 39) Write a function to add new items to a UL
 
-// Write a function to add new items to an UL
 
-const unorderedList = document.createElement("ul")
-newListElement.innerText("Hi I'm the new guy here")
+// 40) Write a function to empty a list
+function emptyUlList() {
+    const ulList = document.querySelector("#container ul")
+
+    ulList.innerHTML = ""
+
+}
+
+
+// DOM 
